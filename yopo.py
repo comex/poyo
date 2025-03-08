4da14410264937ba1b7bb04101033306bf01be6f
comex <comex@comex.local>
2025-03-08 14:27:47 -0800

    up and down

--- yopo.py
+++ yopo.py
@@ -1,18 +1,85 @@
-import http.server
-from http import HTTPStatus
+import socket
+import threading
+import struct
 import time
+import atexit
+from pathlib import Path
+from dataclasses import dataclass
+rc_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#sock.bind(('127.0.0.1', 0))
+rc_sock.connect(('127.0.0.1', 55355))
+pad_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+pad_sock.connect(('127.0.0.1', 55400))
 
-class RequestHandler(http.server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        print(self.headers)
-        self.send_response(HTTPStatus.OK)
-        self.end_headers()
-        self.wfile.write(b'ho\n')
-        time.sleep(1)
+def rc_send(cmd: str):
+    rc_sock.sendall(cmd.encode('utf-8'))
+def rc_send_and_recv(cmd: str) -> str:
+    rc_send(cmd)
+    return rc_sock.recv(65536).decode('utf-8')
+
+screenshot_dir = Path('~/Documents/RetroArch/screenshots')
+def screenshot():
+    rc_send('SCREENSHOT')
+
+@dataclass
+class PadState:
+    up: bool = False
+    down: bool = False
+    left: bool = False
+    right: bool = False
+    a: bool = False
+    b: bool = False
+    start: bool = False
+    select: bool = False
+
+# https://github.com/libretro/RetroArch/blob/master/libretro-common/include/libretro.h#L320
+PAD_STATE_ATTR_TO_RETRO_DEVICE_ID = {
+    'up': 4,
+    'down': 5,
+    'left': 6,
+    'right': 7,
+    'a': 8,
+    'b': 9,
+    'start': 3,
+    'select': 2,
+}
+
+def pad_send(state: PadState):
+    # https://github.com/libretro/RetroArch/blob/9cad6dd993c192030857733d8bd5f27616ece544/cores/libretro-net-retropad/net_retropad_core.c#L81
+    msgs: list[bytes] = []
+    for pad_state_attr, retro_device_id in PAD_STATE_ATTR_TO_RETRO_DEVICE_ID.items():
+        val: bool = getattr(state, pad_state_attr)
+        msgs.append(struct.pack('<IIIIHH',
+            0, # port
+            1, # device = RETRO_DEVICE_JOYPAD
+            0, # index
+            retro_device_id, # id
+            int(val), # state
+            0, # [padding]
+        ))
+    for msg in msgs:
+        pad_sock.send(msg)
+
+def ud_thread():
+    state = PadState()
+    up = True
+    while True:
+        up = not up
+        state.down = not up
+        state.up = up
+        pad_send(state)
+        time.sleep(0.2)
+
+@atexit.register
+def clear_pad():
+    print('clear_pad')
+    pad_send(PadState())
 
 def main():
-    listen = ('127.0.0.1', 25192)
-    server = http.server.ThreadingHTTPServer(listen, RequestHandler)
-    print('listening on', listen)
-    server.serve_forever()
+    threading.Thread(target=ud_thread).start()
+    while True:
+        screenshot()
+        time.sleep(1)
+        
+    pass
 if __name__ == '__main__': main()
